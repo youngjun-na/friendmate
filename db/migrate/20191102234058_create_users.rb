60e3e2233422fc7fd0befbb1d485133fa9d7d98a
youngjunna <yjna4006@gmail.com>
2019-11-02 17:38:36 -0700

    Set up skeleton for clone

---
+++ db/migrate/20191102234058_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :email, null: false
+      t.date :birthday, null: false
+      t.string :gender, null: false
+      t.timestamps
+    end
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+    add_index :users, :first_name
+    add_index :users, :last_name
+  end
+end
