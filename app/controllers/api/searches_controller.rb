051fd9897739f304b36e2f4fed986da6be249f40
youngjunna <yjna4006@gmail.com>
2019-11-13 15:07:42 -0800

    Removed some potential bug-inducing code from search controller

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -1,11 +1,11 @@
 class Api::SearchesController < ApplicationController
-  def index
-    query = '%' + params[:query].to_s +'%'
-    @users = User.where('first_name ILIKE ? OR last_name ILIKE ?', query, query).limit(5)
-    @posts = Post.where('body ILIKE ?', query).limit(5)
-    @query = params[:query]
-    render :index
-  end
+  # def index
+  #   query = '%' + params[:query].to_s +'%'
+  #   @users = User.where('first_name ILIKE ? OR last_name ILIKE ?', query, query).limit(5)
+  #   @posts = Post.where('body ILIKE ?', query).limit(5)
+  #   @query = params[:query]
+  #   render :index
+  # end
 
   
 end
